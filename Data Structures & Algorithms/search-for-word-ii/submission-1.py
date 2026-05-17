9872afa4c56773377e8848fe21679309812a52fb
Farah Abdi <122993476+Aabdi22k@users.noreply.github.com>
2026-05-17 12:59:20 -0400

    Add: search-for-word-ii - submission-1

---
+++ Data Structures & Algorithms/search-for-word-ii/submission-1.py
@@ -0,0 +1,59 @@
+class TrieNode:
+    def __init__(self):
+        self.idx = -1
+        self.children = [None] * 26
+                
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        root = TrieNode()
+
+        def addWord(word: str, i) -> None:
+            cur = root
+            for ch in word:
+                idx = ord(ch) - ord('a')
+                if not cur.children[idx]:
+                    cur.children[idx] = TrieNode()
+                cur = cur.children[idx]
+            cur.idx = i
+    
+
+        for i, word in enumerate(words):
+            addWord(word, i)
+        
+
+        ROWS, COLS = len(board), len(board[0])
+        res = []
+
+        def getIndex(ch):
+            return ord(ch) - ord('a')
+        
+        def backtrack(r, c, cur):
+            if (r < 0 or c < 0 or r >= ROWS or c >= COLS or board[r][c] == '#' or 
+                not cur.children[getIndex(board[r][c])]): return
+
+            tmp = board[r][c]
+            board[r][c] = '#'
+
+            cur = cur.children[getIndex(tmp)] 
+            if cur.idx != -1:
+                res.append(words[cur.idx])
+                cur.idx = -1
+
+
+            backtrack(r + 1, c, cur)
+            backtrack(r - 1, c, cur)
+            backtrack(r, c + 1, cur)
+            backtrack(r, c - 1, cur)
+
+            board[r][c] = tmp
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                backtrack(r, c, root)
+
+
+        return res
+
+
+
